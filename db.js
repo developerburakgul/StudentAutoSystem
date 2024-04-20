dd2c925cac162e3ce44d8f04953522a4c6ff7405
Emirhan Baran <emir.baran255@gmail.com>
2024-04-21 02:34:12 +0300

    Improved we are getting db info frm .env file

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg")
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'studenauto',
-  password: '1234',
-  port: 5432
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: DB_PORT
 })
 
 module.exports = {pool}
